761b825e2f5899b3a8954d610c6e19ac3e947127
camilo520 <camilo.cuevas@uao.edu.co>
2020-10-08 21:23:52 -0500

    cambios interfaz

---
+++ Smart_City/Assets/Scenes/Scripts/boton_apartamento.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boton_apartamento : MonoBehaviour
+{
+
+    public void changescene(string scene)
+    {
+        Application.LoadLevel(scene);
+    }
+}
---
+++ Smart_City/Assets/cursor.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class cursor : MonoBehaviour , IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+
+    public Texture2D hand;
+    public Animator anim;
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+    }
+
+    public void OnPointerClick(PointerEventData eventDat)
+    {
+        if (name.Equals("apartamentos1"))
+        {
+            anim.SetTrigger("Activate");
+        }
+    }
+    
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        print("OnMouseEnter");
+        Cursor.SetCursor(hand, Vector2.zero, CursorMode.ForceSoftware);
+
+    }
+
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        print("OnMouseExit");
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.ForceSoftware);
+    }
+
+
+
+}
