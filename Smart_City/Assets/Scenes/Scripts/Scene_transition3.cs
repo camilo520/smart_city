d6a2eacc0e43cbc4f56fee5d4254f23da26ffc76
camilo520 <camilo.cuevas@uao.edu.co>
2020-10-10 11:50:39 -0500

    animacion correcion1

---
+++ Smart_City/Assets/Scenes/Scripts/Scene_transition3.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene_transition3 : MonoBehaviour
+{
+    public Animator transitionanim;
+    public string scenename;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(LoadScene());
+        }
+    }
+
+    IEnumerator LoadScene()
+    {
+        transitionanim.SetTrigger("end");
+        yield return new WaitForSeconds(0.1f);
+        SceneManager.LoadScene(scenename);
+    }
+}
