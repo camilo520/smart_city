1c1a5d610405422ead18689e872fb7f0589cab5b
camilo520 <camilo.cuevas@uao.edu.co>
2020-10-28 21:41:24 -0500

    com 2.0

---
+++ Smart_City/Assets/Scenes/ejecutivo/ButtonEnableEjecutivo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonEnableEjecutivo : MonoBehaviour
+{
+    public Button continuar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        continuar.interactable = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (DropSlotEjecutivo1.encasilla == true && DropSlotEjecutivo2.encasilla == true && DropSlotEjecutivo3.encasilla == true)
+        {
+            continuar.interactable = true;
+        }
+        else
+        {
+            continuar.interactable = false;
+        }
+    }
+}
