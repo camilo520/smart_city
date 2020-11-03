50b895d24f3a06f4b36104ad89f859cfd2937b3b
camilo520 <camilo.cuevas@uao.edu.co>
2020-11-03 11:37:20 -0500

    2.0

---
+++ Smart_City/Assets/Scenes/casas/ventanaEmergente.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ventanaEmergente : MonoBehaviour
+{
+    public GameObject panel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void openPanel()
+    {
+        if(panel != null)
+        {
+            panel.SetActive(true);
+        }
+    }
+
+    public void closePanel()
+    {
+        if(panel != null)
+        {
+            panel.SetActive(false);
+        }
+    }
+}
