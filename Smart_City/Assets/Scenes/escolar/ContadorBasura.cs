380a542d39dc24ebe7087ff38708830ec75dc67f
camilo520 <camilo.cuevas@uao.edu.co>
2020-11-18 18:01:00 -0500

    sisas

---
+++ Smart_City/Assets/Scenes/escolar/ContadorBasura.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ContadorBasura : MonoBehaviour
+{
+
+    public Text contador;
+    private float tiempo = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        contador.text = " " + tiempo;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tiempo += Time.deltaTime;
+        contador.text = " " + tiempo.ToString("f0");
+        /*if(tiempo>=5 && tiempo <= 10)
+        {
+            Debug.Log("Bien");
+        }
+        else if (tiempo >= 11 && tiempo <= 20)
+        {
+            Debug.Log("Mal");
+        }
+        else if(tiempo >=21)
+        {
+            Debug.Log("En la inmunda");
+
+        }*/
+    }
+
+}
---
+++ Smart_City/Assets/Scenes/industrial/posicionHumo.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class posicionHumo : MonoBehaviour, IDragHandler, IEndDragHandler, IBeginDragHandler
+{
+
+    public static GameObject humo;
+    Vector3 posicioninicial;
+    Transform startParent;
+    Transform dragParent;
+    private int posy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dragParent = GameObject.FindGameObjectWithTag("DragParent").transform;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(humo.transform.position.y);
+        
+        Debug.Log(posy);
+        contaminado();
+    }
+
+    
+
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+
+        humo = gameObject;
+        posicioninicial = transform.position;
+        startParent = transform.parent;
+        transform.SetParent(dragParent);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+
+        humo = null;
+
+        /*if (transform.parent == dragParent)
+        {
+            transform.position = posicioninicial;
+            transform.SetParent(startParent);
+        }*/
+
+        transform.SetParent(dragParent);
+    }
+
+    private void contaminado()
+    {
+        humo = gameObject;
+        Int32.TryParse(humo.transform.position.y.ToString(), out posy);
+        if (posy >= 255)
+        {
+            Debug.Log("Esta en el cielo");
+        }
+        else if (posy >= 170 && posy<255)
+        {
+            Debug.Log("Esta en los edificios");
+        }
+        else if (posy<170 && posy>=0)
+        {
+            Debug.Log("En la calle");
+        }
+        else
+        {
+            Debug.Log("En el infierno");
+        }
+    }
+}
