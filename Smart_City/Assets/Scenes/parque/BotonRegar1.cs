a6b25a96c1f21359b2c033ebaeb13fce411bcfa4
camilo520 <camilo.cuevas@uao.edu.co>
2020-12-12 18:11:26 -0500

    ...

---
+++ Smart_City/Assets/Scenes/parque/BotonRegar1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BotonRegar1 : MonoBehaviour
+{
+    public Button recogio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn = recogio.GetComponent<Button>();
+        btn.onClick.AddListener(recoger);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void recoger()
+    {
+        ContadorHumedad.verdadero=false;
+        //Debug.Log("contador1 " + contador);
+    }
+}
--- Smart_City/Assets/Scenes/parque/ContadorHumedad.cs
+++ Smart_City/Assets/Scenes/parque/ContadorHumedad.cs
@@ -8,38 +8,70 @@ public class ContadorHumedad : MonoBehaviour
 
     public Text humedad1;
     public static float tiempo;
+    public static bool verdadero;
+    private bool sepaso;
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(contadorHume1());
+        tiempo = 600f;
+        StartCoroutine(sumarHume());
+        StartCoroutine(restarHume());
         humedad1.text = "";
+        verdadero = true;
+        sepaso = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("es asi: " + sepaso);
+        if(tiempo>=550f && tiempo <=600f)
+        {
+            sepaso = false;
+            verdadero = true;
+        }
+        else if(tiempo>750f && tiempo <= 1000f)
+        {
+            sepaso = true;
+        }
+        Debug.Log("sepaso: " + sepaso);
     }
 
-    IEnumerator contadorHume1()
+    IEnumerator sumarHume()
     {
-        tiempo = 600f;
-        while (tiempo<1000f)
+        while (true)
         {
-            if (DropSlotParque.encasilla == true && DropSlotParque2.encasilla == true )
+            if (DropSlotParque.encasilla == true && DropSlotParque2.encasilla == true && verdadero == true)
             {
-                tiempo += Random.Range(15, 20);
+                tiempo += Random.Range(25, 30);
                 humedad1.text = " " + tiempo.ToString("f0");
-
             }
             else
             {
-
             }
             yield return new WaitForSeconds(2);
-
-
         }
 
     }
+
+    IEnumerator restarHume()
+    {
+        while (true)
+        {
+            if (DropSlotParque.encasilla == true && DropSlotParque2.encasilla == true && verdadero == false && sepaso==true)
+            {
+                tiempo -= Random.Range(25, 30);
+                humedad1.text = " " + tiempo.ToString("f0");
+            }
+            else
+            {
+                
+            }
+            yield return new WaitForSeconds(2);
+        }
+
+    }
+
+
+
 }
