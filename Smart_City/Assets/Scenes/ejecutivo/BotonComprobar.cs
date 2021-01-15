6d4cb7b34bd547c71794485524ee25f59227b7c2
camilo520 <camilo.cuevas@uao.edu.co>
2021-01-15 12:12:27 -0500

    :angel:

--- Smart_City/Assets/Scenes/ejecutivo/BotonComprobar.cs
+++ Smart_City/Assets/Scenes/ejecutivo/BotonComprobar.cs
@@ -7,11 +7,13 @@ public class BotonComprobar : MonoBehaviour
 {
     public Button comprobar;
     public GameObject panel;
+    public GameObject panel2;
     public GameObject textoAviso;
     // Start is called before the first frame update
     void Start()
     {
         panel.SetActive(false);
+        panel2.SetActive(false);
         textoAviso.SetActive(false);
         Button btn = comprobar.GetComponent<Button>();
         btn.onClick.AddListener(comprobarCasos);
@@ -33,26 +35,36 @@ public class BotonComprobar : MonoBehaviour
             && BotonEspacio3.disponible == true && BotonEspacio4.disponible == false && BotonEspacio5.disponible == true)
         {
             panel.SetActive(true);
+            panel2.SetActive(false);
+        }
+        else
+        {
+            panel.SetActive(false);
+            panel2.SetActive(true);
         }
         if (CasosParqueadero.caso4 == true && BotonEspacio1.disponible == false && BotonEspacio2.disponible == true
             && BotonEspacio3.disponible == false && BotonEspacio4.disponible == true && BotonEspacio5.disponible == false)
         {
             panel.SetActive(true);
+            panel2.SetActive(false);
         }
         if (CasosParqueadero.caso3 == true && BotonEspacio1.disponible == true && BotonEspacio2.disponible == true
             && BotonEspacio3.disponible == false && BotonEspacio4.disponible == false && BotonEspacio5.disponible == false)
         {
             panel.SetActive(true);
+            panel2.SetActive(false);
         }
         if (CasosParqueadero.caso2 == true && BotonEspacio1.disponible == false && BotonEspacio2.disponible == true
             && BotonEspacio3.disponible == true && BotonEspacio4.disponible == true && BotonEspacio5.disponible == false)
         {
             panel.SetActive(true);
+            panel2.SetActive(false);
         }
         if (CasosParqueadero.caso1 == true && BotonEspacio1.disponible == true && BotonEspacio2.disponible == false
             && BotonEspacio3.disponible == false && BotonEspacio4.disponible == false && BotonEspacio5.disponible == true)
         {
             panel.SetActive(true);
+            panel2.SetActive(false);
         }
     }
 }
