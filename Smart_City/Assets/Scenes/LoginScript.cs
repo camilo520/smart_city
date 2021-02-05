bce6181f5e83566f84fa9634095c07c2f838aa15
Camilo Cuevas <camilo.cuevas@uao.edu.co>
2021-02-05 11:40:09 -0500

    :airplane:

---
+++ Smart_City/Assets/Scenes/LoginScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoginScript : MonoBehaviour
+{
+    public InputField login;
+    public InputField pass;
+    public Text aviso;
+    // Start is called before the first frame update
+    void Start()
+    {
+        aviso.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void botonLog()
+    {
+        if (login.text.Equals("juanpis") && pass.text.Equals("12345"))
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+        else
+        {
+            StartCoroutine(avisoS());
+        }
+    }
+
+    IEnumerator avisoS()
+    {
+        aviso.text = "Usuario/Contraseña Incorrecta";
+
+        yield return new WaitForSeconds(2);
+
+        aviso.text = "";
+
+        yield return new WaitForSeconds(2);
+
+    }
+}
