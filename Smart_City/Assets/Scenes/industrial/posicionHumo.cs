317c34dd6ce1696ebb8e08c439b72861c3a76fa8
Camilo Cuevas <camilo.cuevas@uao.edu.co>
2021-08-18 19:56:40 -0500

    final 2.5

--- Smart_City/Assets/Scenes/industrial/posicionHumo.cs
+++ Smart_City/Assets/Scenes/industrial/posicionHumo.cs
@@ -68,7 +68,7 @@ public class posicionHumo : MonoBehaviour, IDragHandler, IEndDragHandler, IBegin
         humo = gameObject;
         Int32.TryParse(humo.transform.position.y.ToString(), out posy);
 
-        if (posy >= 500)
+        if (posy >= 550)
         {
             humo1 = true;
         }
